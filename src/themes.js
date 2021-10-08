4255371aafcff3d9176a49bb78ac36487ce7249c
Maria <mfb171201@gmail.com>
2021-10-08 12:26:59 -0400

    atividade feita

---
+++ src/themes.js
@@ -0,0 +1,23 @@
+const lightTheme = {
+    color: "#0e14e",
+    backgroundColor: "#d3d4e6",
+    button: {
+        textColor: "#d3d4e6",
+        borderColor: "#151a69",
+        backgroundColor: "#151a69"
+    },
+}
+const darkTheme = {
+    color: "#d3d4e6",
+    backgroundColor: "#39424e",
+    button: {
+        textColor: "#151a69",
+        borderColor: "#151a69",
+        backgroundColor: "#39424e"
+    }
+}
+
+export const themes = {
+    light: lightTheme,
+    dark: darkTheme,
+}
